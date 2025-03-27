f6e2ef9c49d9305a75cc3ac63029f05d0059374f
Ramesh <rameshrathiakg@gmail.com>
2025-03-27 08:20:36 +0530

    fix

--- libbm/src/main.c
+++ libbm/src/main.c
@@ -1,8 +1,39 @@
+// src/main.c
+// main file for libbm
 
-int main()
-{
-	
-	
-	
-	return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <command> [args...]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    pid_t pid = fork(); // fork a child process and run command in child process
+    if (pid < 0) {
+        perror("fork failed");
+        return EXIT_FAILURE;
+    }
+
+    if (pid == 0) {
+        // Child process: Execute the command
+        execvp(argv[1], &argv[1]);
+        perror("execvp failed");
+        exit(EXIT_FAILURE);
+    } else {
+        // Parent process: Wait for the child to complete
+        int status;
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status)) {
+            printf("Command exited with status %d\n", WEXITSTATUS(status));
+        } else {
+            printf("Command did not terminate normally\n");
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
