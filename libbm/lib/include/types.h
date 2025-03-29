66a19bc998d89d1a0e51870800ac6f822f4c49a1
Ramesh <rameshrathiakg@gmail.com>
2025-03-29 09:02:27 +0000

    nice one

--- libbm/lib/include/types.h
+++ libbm/lib/include/types.h
@@ -15,17 +15,17 @@
 typedef     unsigned char           uint8_t;
 typedef     unsigned short          uint16_t;
 typedef     unsigned int            uint32_t;
-typedef     unsigned long long      uint64_t;
+typedef     long unsigned int       uint64_t;
 
 typedef     short                   int16_t;
 typedef     int                     int32_t;
-typedef     long long               int64_t;
+typedef     long int               int64_t;
 
 
 #if defined(__arm64__) || defined(__x64__)
 typedef     unsigned long    size_t;    // For 64 bits machine
 #else
-typedef     unsigned int     size_t;    // For 32 bits machine
+typedef     unsigned long     size_t;    // For 32 bits machine
 #endif
 
 #endif
--- libbm/main.c
+++ libbm/main.c
@@ -1,13 +1,23 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "lib/include/bmlib.h"
+
+#define FILE_NAME "Package.swift"
+
 int main()
 {
-
-
-    while (1)
-    {
-        /* code */
-    }
-    
+	int fd;
+	fd = open(FILE_NAME, O_RDONLY);
+	if (fd < 0) {
+		perror("open");
+		exit(1);
+	}
+	
+	printf("\n");
     return 0;
 
 }
