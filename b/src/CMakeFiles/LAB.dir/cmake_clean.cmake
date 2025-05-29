ffc03196ba6405c25509b9e47774b43203c65008
rameshrathi <rameshrathiakg@gmail.com>
2025-05-29 07:36:50 +0000

    init

---
+++ b/src/CMakeFiles/LAB.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LAB.dir/main.cpp.o"
+  "CMakeFiles/LAB.dir/main.cpp.o.d"
+  "LAB"
+  "LAB.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LAB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
