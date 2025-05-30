29867e63953519acb6ccba07aa0662e44618f516
ramesh kumar <rameshrathiakg@gmail.com>
2025-05-30 18:10:44 +0530

    Adding script

---
+++ lab.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+IFS=$'\n\t'
+
+BUILD_DIR="build"
+ELF_FILE="$BUILD_DIR/Run/RUN"
+
+# Build the project
+build() {
+	echo "[+] building project..."
+	cmake -S . -B "$BUILD_DIR"
+	cmake --build "$BUILD_DIR"
+}
+
+# Run all tests
+test() {
+	echo "[+] running tests..."
+	if [[ ! -d "$BUILD_DIR" ]]; then
+		echo "[!] Build directory not found. Run './lab build' first."
+		exit 1
+	fi
+	cd "$BUILD_DIR/Tests"
+	ctest --output-on-failure
+}
+
+# Run app
+run() {
+	echo "[+] running application..."
+  if [[ ! -f "$ELF_FILE" ]]; then
+      echo "[!] Executable not found at : $ELF_FILE  => Run './script.sh build' first."
+      exit 1
+  fi
+  "$ELF_FILE"
+}
+
+usage() {
+	echo "Usage : $0 { build | test | run }"
+	exit 1
+}
+
+main() {
+	[[ $# -eq 0 ]] && usage
+	case "$1" in
+	build) build;;
+	test) test;;
+	run) run;;
+	*) usage;;
+	esac
+}
+
+main "$@"
